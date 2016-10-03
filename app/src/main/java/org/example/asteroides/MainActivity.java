198b8a14f7ce170445d4672954ae22af4c756c2c
José Alberto Martín <jalbertomartinfalcon@gmail.com>
2016-10-03 13:30:09 +0200

    adding menu

--- app/src/main/java/org/example/asteroides/MainActivity.java
+++ app/src/main/java/org/example/asteroides/MainActivity.java
@@ -3,6 +3,8 @@ package org.example.asteroides;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -39,5 +41,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void exitApplication() {
         finish();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true; /** true -> el menú ya está visible */}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.about_menu) {
+            throwAboutActivity(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
